da836e734bf8b95797ac012270f159187370e435
sdwang <siwang22@gmail.com>
2015-10-16 15:32:31 -0700

    (add) svg container. circles. work on stopping circles from crossing border

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,65 @@
 // start slingin' some d3 here.
+
+var sizes = [5, 5, 5, 10, 10, 10, 15, 15, 20, 25];
+
+var w = 960;
+var h = 500;
+
+var svg = d3.select("body")
+  .append("svg")
+  .attr("width", w)
+  .attr("height", h)
+  .attr("position", 'relative');
+
+var circles = svg.selectAll("circle")
+  .data(sizes)
+  .enter()
+  .append("circle");
+
+function generateLocation(d, max) {
+  var loc = Math.random() * max;
+  
+  if (loc >=max) {
+    return loc-d;
+  } else if (loc <=0) {
+    return loc + d;
+  }
+  
+  return loc;
+
+};
+
+circles.attr("r", function(d) {
+  return d;
+  })
+  .attr("cx", generateLocation(d, w))
+  .attr("cy", generateLocation(d, h))
+  .attr("position", 'absolute');
+
+
+  
+//Width and height
+// var w = 500;
+// var h = 50;
+
+// //Data
+// var dataset = [ 5, 10, 15, 20, 25 ];
+
+// //Create SVG element
+// var svg = d3.select("body")
+//       .append("svg")
+//       .attr("width", 500)
+//       .attr("height", 50);
+
+// var circles = svg.selectAll("circle")
+//     .data(dataset)
+//     .enter()
+//     .append("circle");
+
+// circles.attr("cx", function(d, i) {
+//       return (i * 50) + 25;
+//     })
+//      .attr("cy", h/2)
+//      .attr("r", function(d) {
+//       return d;
+//      });
