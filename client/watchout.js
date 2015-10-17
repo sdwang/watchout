94e20fa91192cec0c36cfff3e29c51138644baf7
sdwang <siwang22@gmail.com>
2015-10-16 20:05:50 -0700

    (add) User can click and drag player

--- client/watchout.js
+++ client/watchout.js
@@ -15,21 +15,30 @@ var svg = d3.select("body")
 ----Enemies-----
 */
 
-var sizes = [5, 5, 5, 6, 7, 8, 10, 10, 11, 12, 15, 18, 20, 24, 25];
+var enemyData = [];
+var numEnemies = 10;
 
+for (var i =0; i < numEnemies; i++) {
+  var enemyObj = {};
+  enemyObj.name = 'enemy' + i;
+  enemyObj.radius = Math.random() * 20 + 5;
+  enemyData.push(enemyObj);
+}
 
 var enemies = svg.selectAll("circle")
-  .data(sizes)
+  .data(enemyData, function(d) {
+    return d.name;
+  })
   .enter()
   .append("circle");
 
-function generateLocation(r, max) {
+function generateLocation(radius, max) {
   var loc = Math.random() * max;
   
-  if (loc >= (max - r)) {
-    return loc-r;
-  } else if (loc <= r) {
-    return loc + r;
+  if (loc >= (max - radius)) {
+    return loc-radius;
+  } else if (loc <= radius) {
+    return loc + radius;
   }
   
   return loc;
@@ -37,52 +46,63 @@ function generateLocation(r, max) {
 };
 
 enemies.attr("r", function(d) {
-  return d;
+  return d.radius;
   })
   .attr("cx", function(d) {
-    return generateLocation(d, w)
+    return generateLocation(d.radius, w)
   })
   .attr("cy", function(d) {
-    return generateLocation(d, h)
+    return generateLocation(d.radius, h)
   })
   .attr("position", 'absolute')
   .attr("class", 'enemies');
 
 
 var makeEnemies = function() {
-  var enemySelector = svg.selectAll('.enemies').data(sizes);
+  var enemySelector = svg.selectAll('.enemies').data(enemyData, function(d) {
+    return d.name
+  });
 
   enemySelector.transition().duration(1000).attr("cx", function(d) {
-    return generateLocation(d, w);
+    return generateLocation(d.radius, w);
   }).attr("cy", function(d) {
-    return generateLocation(d, h);
+    return generateLocation(d.radius, h);
   });
 
   setTimeout(makeEnemies,1000);
 };
 
-//makeEnemies();
+makeEnemies();
   
 /*
 ----Player----
 */
 
-var playerData = [10];
+var playerData = [{radius: 15, name: 'player'}];
 
 var player = svg.selectAll("circle")
-  .data(playerData)
+  .data(playerData, function(d) { return d.name;})
   .enter()
   .append("circle");
 
+var drag = d3.behavior.drag()
+  .on("drag", function(d) {
+    player.attr("cx", d3.event.x)
+    .attr("cy", d3.event.y)
+});
+
 player.attr("r", function(d) {
-  return d;
+  return d.radius;
   })
   .attr("cx", w/2)
   .attr("cy", h/2)
   .attr("position", 'absolute')
-  .attr("class", 'player');
-
-
+  .attr("class", 'player')
+  .attr("fill","yellow")
+  .attr("stroke","orange")
+  .attr("stroke-width", function(d) {return d.radius / 2})
+  .style("cursor", "pointer")
+  .call(drag);
 
 
 
